5e030fb9daf0512a8d78ccbbe23b8385ac8d4085
Vinicius Soares <vinispmm@hotmail.com>
2025-04-21 17:30:45 -0300

    commit

--- src/core/game.js
+++ src/core/game.js
@@ -5,6 +5,7 @@ import { battleZones } from '../maps/battleZones.js';
 
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
+
 canvas.width = 1024;
 canvas.height = 576;
 
